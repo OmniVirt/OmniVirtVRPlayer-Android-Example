107348b354262cc0b039ceaff37bc32632650f0e
nuuneoi <sittiphol@gmail.com>
2017-06-14 13:32:24 -0700

    Use new SDK interface

--- app/src/main/java/com/omnivirt/vrkitexample/PlayerActivity.java
+++ app/src/main/java/com/omnivirt/vrkitexample/PlayerActivity.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.games.Player;
 import com.omnivirt.vrkit.AdState;
 import com.omnivirt.vrkit.Mode;
 import com.omnivirt.vrkit.OnVRAdInteractionListener;
@@ -18,11 +19,12 @@ import com.omnivirt.vrkit.OnVRPlayerInteractionListener;
 import com.omnivirt.vrkit.Quality;
 import com.omnivirt.vrkit.VRAd;
 import com.omnivirt.vrkit.VRPlayerFragment;
+import com.omnivirt.vrkit.VRPlayerSupportFragment;
 
 import java.lang.reflect.Array;
 
-public class PlayerActivity extends AppCompatActivity implements OnVRPlayerInteractionListener, OnVRAdInteractionListener {
-    private VRPlayerFragment mPlayer = null;
+public class PlayerActivity extends AppCompatActivity implements OnVRPlayerInteractionListener {
+    private VRPlayerSupportFragment mPlayer = null;
     private TextView mTextView = null;
     private Button mAdButton = null;
     private VRAd mVrAd = null;
@@ -32,9 +34,9 @@ public class PlayerActivity extends AppCompatActivity implements OnVRPlayerInter
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
 
-        mTextView = (TextView)this.findViewById(R.id.vrplayer_output_textview); // Player State Logging
+        mTextView = (TextView) this.findViewById(R.id.vrplayer_output_textview); // Player State Logging
 
-        mPlayer = (VRPlayerFragment)this.getSupportFragmentManager().findFragmentById(R.id.vrplayer_fragment);
+        mPlayer = (VRPlayerSupportFragment) this.getSupportFragmentManager().findFragmentById(R.id.vrplayer_fragment);
 
         // Use this method to disable interface
         //
@@ -46,18 +48,18 @@ public class PlayerActivity extends AppCompatActivity implements OnVRPlayerInter
 
         // Creating VR Ad instance
         //
-        mVrAd = new VRAd(1, this);
+        mVrAd = new VRAd(1, vrAdInteractionListener);
 
         // Loading and starting VR Ad using a button
         //
-        mAdButton = (Button)this.findViewById(R.id.start_ad_button);
+        mAdButton = (Button) this.findViewById(R.id.start_ad_button);
         mAdButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 String title = mAdButton.getText().toString();
                 if (title.equals("Load Ad")) {
                     // Load Ad
                     //
-                    mVrAd.load();
+                    mVrAd.load(PlayerActivity.this);
                 } else if (title.equals("Start Ad")) {
                     // Select the option to turn on / off Cardboard mode for ads
                     //
@@ -86,53 +88,6 @@ public class PlayerActivity extends AppCompatActivity implements OnVRPlayerInter
     }
 
     @Override
-    public void onAdStatusChanged(VRAd instance, AdState status) {
-        switch (status) {
-            case None:
-                break;
-            case Loading:
-                this.log("Ad state is loading");
-                mAdButton.setText("Loading Ad");
-                mAdButton.setEnabled(false);
-                break;
-            case Ready:
-                this.log("Ad state is ready");
-                mAdButton.setText("Start Ad");
-                mAdButton.setEnabled(true);
-                break;
-            case Showing:
-                this.log("Ad state is showing");
-                mAdButton.setText("Showing Ad");
-                mAdButton.setEnabled(false);
-                mPlayer.setIdle(Mode.ON); // Idling any video player to reserve GPU resources for VR Ad
-                break;
-            case Completed:
-                this.log("Ad state is completed");
-                mAdButton.setText("Load Ad");
-                mAdButton.setEnabled(true);
-                mPlayer.setIdle(Mode.OFF); // Resume the video player
-                break;
-            case Failed:
-                this.log("Ad state is failed");
-                mAdButton.setText("Load Ad");
-                mAdButton.setEnabled(true);
-                break;
-        }
-    }
-
-    @Override
-    public void onFragmentCreated() {
-        mPlayer.load(24);
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration config) {
-        // This method helps player from reloading when orientation changes.
-        //
-        super.onConfigurationChanged(config);
-    }
-
-    @Override
     public void onBackPressed() {
         // This method will close VR Ad when user hit back button.
         //
@@ -146,69 +101,121 @@ public class PlayerActivity extends AppCompatActivity implements OnVRPlayerInter
         super.onBackPressed();
     }
 
+    ////////////////////////
+    // Listener for VRAd
+    ////////////////////////
+
+    private OnVRAdInteractionListener vrAdInteractionListener = new OnVRAdInteractionListener() {
+        @Override
+        public void onAdStatusChanged (VRAd instance, AdState status){
+            switch (status) {
+                case None:
+                    break;
+                case Loading:
+                    PlayerActivity.this.log("Ad state is loading");
+                    mAdButton.setText("Loading Ad");
+                    mAdButton.setEnabled(false);
+                    break;
+                case Ready:
+                    PlayerActivity.this.log("Ad state is ready");
+                    mAdButton.setText("Start Ad");
+                    mAdButton.setEnabled(true);
+                    break;
+                case Showing:
+                    PlayerActivity.this.log("Ad state is showing");
+                    mAdButton.setText("Showing Ad");
+                    mAdButton.setEnabled(false);
+                    mPlayer.setIdle(Mode.ON); // Idling any video player to reserve GPU resources for VR Ad
+                    break;
+                case Completed:
+                    PlayerActivity.this.log("Ad state is completed");
+                    mAdButton.setText("Load Ad");
+                    mAdButton.setEnabled(true);
+                    mPlayer.setIdle(Mode.OFF); // Resume the video player
+                    break;
+                case Failed:
+                    PlayerActivity.this.log("Ad state is failed");
+                    mAdButton.setText("Load Ad");
+                    mAdButton.setEnabled(true);
+                    break;
+            }
+        }
+    };
+
+    ////////////////////////////
+    // Listener for VRPlayer
+    ////////////////////////////
+
     @Override
-    public void onLoaded(Integer maxQuality, Quality quality, Mode mode) {
+    public void onVRPlayerFragmentCreated() {
+        mPlayer.load(24);
+    }
+
+    @Override
+    public void onVRPlayerLoaded(Integer maxQuality, Quality quality, Mode mode) {
         this.log("Loaded maxQuality:" + maxQuality.toString() + " currentQuality:" + quality.toString() + " currentMode:" + mode.toString());
     }
 
     @Override
-    public void onStarted() {
+    public void onVRPlayerStarted() {
         this.log("Started");
     }
 
     @Override
-    public void onPaused() {
+    public void onVRPlayerPaused() {
         this.log("Paused");
     }
 
     @Override
-    public void onEnded() {
+    public void onVRPlayerEnded() {
         this.log("Ended");
     }
 
     @Override
-    public void onSkipped() { this.log("Skipped"); }
-
-    @Override
-    public void onDurationChanged(Double aDouble) {
-        // this.log("Duration changed to " + aDouble.toString());
+    public void onVRPlayerSkipped() {
+        this.log("Skipped");
     }
 
     @Override
-    public void onProgressChanged(Double aDouble) {
-        // this.log("Progress changed to " + aDouble.toString());
+    public void onVRPlayerDurationChanged(Double aDouble) {
+        this.log("Duration changed to " + aDouble.toString());
     }
 
     @Override
-    public void onBufferChanged(Double aDouble) {
-        // this.log("Buffer changed to " + aDouble.toString());
+    public void onVRPlayerProgressChanged(Double aDouble) {
+        this.log("Progress changed to " + aDouble.toString());
     }
 
     @Override
-    public void onSeekChanged(Double aDouble) {
-        // this.log("Seek changed to " + aDouble.toString());
+    public void onVRPlayerBufferChanged(Double aDouble) {
+        this.log("Buffer changed to " + aDouble.toString());
     }
 
     @Override
-    public void onCardboardChanged(Mode mode) {
+    public void onVRPlayerSeekChanged(Double aDouble) {
+        this.log("Seek changed to " + aDouble.toString());
+    }
+
+    @Override
+    public void onVRPlayerCardboardChanged(Mode mode) {
         this.log("Cardboard changed to " + mode.toString());
     }
 
     @Override
-    public void onAudioChanged(Double aDouble) {
+    public void onVRPlayerAudioChanged(Double aDouble) {
         this.log("Audio changed to " + aDouble.toString());
     }
 
     @Override
-    public void onQualityChanged(Quality quality) {
+    public void onVRPlayerQualityChanged(Quality quality) {
         this.log("Quality changed to " + quality.toString());
     }
 
     @Override
-    public void onExpanded() {
+    public void onVRPlayerExpanded() {
         this.log("Expanded");
-        final LinearLayout layout = (LinearLayout)this.findViewById(R.id.vrplayer_container);
-        final LinearLayout.LayoutParams params = (LinearLayout.LayoutParams)layout.getLayoutParams();
+        final LinearLayout layout = (LinearLayout) this.findViewById(R.id.vrplayer_container);
+        final LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) layout.getLayoutParams();
         params.height = LinearLayout.LayoutParams.MATCH_PARENT;
         layout.post(new Runnable() {
             @Override
@@ -219,10 +226,10 @@ public class PlayerActivity extends AppCompatActivity implements OnVRPlayerInter
     }
 
     @Override
-    public void onCollapsed() {
+    public void onVRPlayerCollapsed() {
         this.log("Collapsed");
-        final LinearLayout layout = (LinearLayout)this.findViewById(R.id.vrplayer_container);
-        final LinearLayout.LayoutParams params = (LinearLayout.LayoutParams)layout.getLayoutParams();
+        final LinearLayout layout = (LinearLayout) this.findViewById(R.id.vrplayer_container);
+        final LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) layout.getLayoutParams();
         params.height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 300, getResources().getDisplayMetrics());
         layout.post(new Runnable() {
             @Override
@@ -233,17 +240,17 @@ public class PlayerActivity extends AppCompatActivity implements OnVRPlayerInter
     }
 
     @Override
-    public void onLatitudeChanged(Double aDouble) {
-        // this.log("Latitude changed to " + aDouble.toString());
+    public void onVRPlayerLatitudeChanged(Double aDouble) {
+        this.log("Latitude changed to " + aDouble.toString());
     }
 
     @Override
-    public void onLongitudeChanged(Double aDouble) {
-        // this.log("Longitude changed to " + aDouble.toString());
+    public void onVRPlayerLongitudeChanged(Double aDouble) {
+        this.log("Longitude changed to " + aDouble.toString());
     }
 
     @Override
-    public void onSwitched(String s, Array array) {
+    public void onVRPlayerSwitched(String s, Array array) {
         this.log("Swicthed scene to " + s);
     }
 
